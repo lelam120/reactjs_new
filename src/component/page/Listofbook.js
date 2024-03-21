fe052d732457da99b3d4c839349e0ea5e7be81a4
lelam120 <lhlsl12@gmail.com>
2024-03-21 20:24:11 +0700

    new

--- src/component/page/Listofbook.js
+++ src/component/page/Listofbook.js
@@ -25,17 +25,58 @@ function Listofbook(props){
     
         <ListGroup.Item className='fix'>
         <div className='left'>
-                trai
+                Life of Pi
             </div>
             <div className='right'>
-                le ba ha lam
+                Davan
+            </div>
+        </ListGroup.Item>
+        <ListGroup.Item className='fix'>
+        <div className='left'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+            <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+            </svg>
+                Học code today
+            </div>
+            <div className='right'>
+                Fpt
+            </div>
+        </ListGroup.Item>
+        <ListGroup.Item className='fix'>
+        <div className='left'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+            <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+            </svg>
+                Learn Angular by Exam   
+            </div>
+            <div className='right'>
+                Fpt-Aptech
+            </div>
+        </ListGroup.Item>
+        <ListGroup.Item className='fix'>
+        <div className='left'>
+                Họ nhà trai
+            </div>
+            <div className='right'>
+                Nguyễn Anh Tú
+            </div>
+        </ListGroup.Item>
+        <ListGroup.Item className='fix'>
+        <div className='left'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+            <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+            </svg>
+                Đôi mắt có lửa
+            </div>
+            <div className='right'>
+                Nguyễn Hùng Sơn
             </div>
         </ListGroup.Item>
     
         </ListGroup>
 
         <Form.Select aria-label="Default select example" style={{marginTop:'20px',marginBottom:'20px'}}>
-            <option>Open this select menu</option>
+            <option>Author Ascending</option>
             <option value="1">One</option>
             <option value="2">Two</option>
             <option value="3">Three</option>
